48ab5fa77cc9968c608e76cb5517a712cdc63b5d
practicas14 <alan220193@gmail.com>
2016-08-22 17:21:38 +0000

    migraciones parte 1

---
+++ database/migrations/2016_08_22_171511_add_category_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorys', function (Blueprint $table) {
+            $table->increments('id');
+            //segundo parametros es la longitud
+            $table->string('name',60);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categorys');
+    }
+}
